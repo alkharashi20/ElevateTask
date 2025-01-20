6e6bfc9bca516211ccfa4a39fd1ff598b8c0ba3a
ahmed <ahmedalkharashi99@gmail.com>
2025-01-20 20:34:04 +0200

    added Home_Screen Page .

--- lib/ui/Home/Home_Screen.dart
+++ lib/ui/Home/Home_Screen.dart
@@ -1,11 +1,41 @@
+import 'package:elevatetask1/core/AppColors.dart';
+import 'package:elevatetask1/domain/di/di.dart';
+import 'package:elevatetask1/ui/Home/Cubit/ProductCubit.dart';
+import 'package:elevatetask1/ui/Home/Cubit/Product_States.dart';
+import 'package:elevatetask1/ui/Home/Product_Deatils/Product_Deatils.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+   HomeScreen({super.key,});
   static const String RouteName='HomeScreen';
-
+ProductCubit data = getIt<ProductCubit>();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return BlocBuilder<ProductCubit,Product_States>(
+        bloc: data..getproducts(),
+          builder: (context,state){
+if(state is IsLoadingProduct_States){
+  return Center(child: CircularProgressIndicator());
+}
+          else if (state is FailureProduct_States){
+            return Text('Error');
+}
+        return GridView.builder(
+          itemCount: data.Pro.length,
+          shrinkWrap: true,
+            scrollDirection: Axis.vertical,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                mainAxisSpacing: 15,
+                crossAxisSpacing: 15,
+
+                crossAxisCount: 2),
+             itemBuilder: (BuildContext context, int index) {
+
+          return ProductDeatils(productEntities: data.Pro[index]);
+
+             });
+        });
   }
 }
